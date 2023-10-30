279333a2e3d369c7629ab84ebbab6af0bb91eef6
Faisal00001 <faisalosman798@gmail.com>
2023-10-30 15:52:41 +0600

    Added products section

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,34 +1,35 @@
+import { useEffect, useState } from "react";
 import "./Products.css";
 
 const Products = () => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data)
+            })
+    }, [])
     return (
         <>
+
+
             <div className="card-list">
-                <a className="card-item">
-                    <img src="images/developer.jpg" alt="Card Image" />
-                    <span className="developer">Developer</span>
-                    <h3>A developer codes software and websites.</h3>
-                    <div className="arrow">
-                        <i className="fas fa-arrow-right card-icon"></i>
-                    </div>
-                </a>
-                <a className="card-item">
-                    <img src="images/designer.jpg" alt="Card Image" />
-                    <span className="designer">Designer</span>
-                    <h3>A designer is a design expert.</h3>
-                    <div className="arrow">
-                        <i className="fas fa-arrow-right card-icon"></i>
-                    </div>
-                </a>
-                <a className="card-item">
-                    <img src="images/editor.jpg" alt="Card Image" />
-                    <span className="editor">Editor</span>
-                    <h3>An editor ensures content quality and accuracy.</h3>
-                    <div className="arrow">
-                        <i className="fas fa-arrow-right card-icon"></i>
-                    </div>
-                </a>
+                {
+                    products.map((product, index) => <a key={index} className="card-item">
+                        <img src={product.image} alt="Card Image" />
+                        <span className="developer">{product.title}</span>
+                        <h3>{product.description}</h3>
+                        <div className="arrow">
+                            <i className="fas fa-arrow-right card-icon"></i>
+                        </div>
+                    </a>)
+                }
+
             </div>
+
+
+
         </>
     );
 };
