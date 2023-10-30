8976463bb5654579034c306ca1664b524b2b9b8a
Faisal00001 <faisalosman798@gmail.com>
2023-10-30 15:18:33 +0600

    Added slider in the home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,18 @@
+import Products from "../../components/Products/Products";
+import Slider from "../../components/Products/Slider/Slider";
 import "./Home.css";
 
 const Home = () => {
     return (
-        <div>
-            <h3 className="products-title">Our Products</h3>
-        </div>
+        <>
+            <Slider></Slider>
+            <div className="products-container">
+
+                <h3 className="products-title">Our Products</h3>
+                <Products></Products>
+            </div>
+        </>
+
     );
 };
 
---
+++ src/components/Products/Slider/Slider.jsx
@@ -0,0 +1,25 @@
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+import 'react-awesome-slider/dist/styles.css';
+import jins from '../../../assets/images/slider/jins.jpg';
+import laptop from '../../../assets/images/slider/laptop.jpg';
+import shoe from '../../../assets/images/slider/shoes.jpg';
+const AutoplaySlider = withAutoplay(AwesomeSlider);
+const Slider = () => {
+    return (
+        <div>
+            <AutoplaySlider animation="cubeAnimation"
+                play={true}
+                cancelOnInteraction={false} // should stop playing on user interaction
+                interval={6000}
+            >
+                <div data-src={shoe} />
+                <div data-src={laptop} />
+                <div data-src={jins} />
+            </AutoplaySlider>
+        </div>
+    );
+};
+
+export default Slider;
